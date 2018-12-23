7d8ca78262304a1883e901f8a597caceb0f00f96
NotoriousRebel <matt12299@yahoo.com>
2018-12-22 19:39:26 -0500

    Started working on recursive method to walk zone files, cleaned up some code.

--- walk.py
+++ walk.py
@@ -3,9 +3,14 @@ import subprocess
 import platform
 import sys
 
+"""
+    A simple program to recursively walk zone files for domains that utilize NSEC
+"""
+
+
 def parseArgs():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d","-D", help="enter -d followed by domain", type=str)
+    parser.add_argument("-d", "-D", help="enter -d followed by domain", type=str)
     args = parser.parse_args()  # get args
     output = []
     if args.d is not None:
@@ -15,16 +20,18 @@ def parseArgs():
         sys.exit(-1)
     return output
 
+
 """
 Helper function to test if host is reachable or not
 @param url 
 @return True or False depending if url is pingable
 """
+
+
 def pingOk(host):
-    output = subprocess.getoutput("ping -{} 1 {}".format('n' if platform.system().lower()=="windows" else 'c', host))
-    print(output)
+    output = subprocess.getoutput("ping -{} 1 {}".format('n' if platform.system().lower() == "windows" else 'c', host))
     for line in output.splitlines():
-        if 'Request timed out' in line or 'Ping request could not find host':
+        if 'Request timed out' in line or 'Ping request could not find host' in line:
             return False
     return True
 
@@ -33,37 +40,53 @@ def mailServers(url):
     command = f'dig {url} MX'
     pass
 
-def dig(target):
-    prefix = 'apple'
-    command = 'dig +dnssec ' + prefix + '.' + target
-    #wsl = windows subsystem for linux
-    output = subprocess.getoutput("wsl.exe " + command)
-    info = []
-    NSEC3_flag = False
-    for line in output.strip().splitlines():
-        if 'NSEC' in line:
-            info.append(line)
-        elif 'NSEC3' in line:
-            NSEC3_flag = True
-            info.append(line)
-        else:
-            continue
-    domains = set()
-    for x in info:
-        for temp in x.split(' '):
-            temp = temp[:len(temp)-1]
-            if target in temp and temp not in domains and temp != target:
-                domains.add(temp.replace(' ',''))
-    for el in domains:
-        print(el)
 
-    #replace all numbers with spaces as well as replace IN AND NSEC
-    #[domains.add(temp) for temp in x.strip().split(' ') if 'arin.net' in temp for x in info]
+def dig(target, prefix, NSEC3_flag, domains, stop_flag):
+    if stop_flag:
+        return sorted(list(domains)), NSEC3_flag
+    else:
+        prefix = 'a'
+        command = 'dig +dnssec ' + prefix + '.' + target
+        # wsl = windows subsystem for linux
+        output = subprocess.getoutput("wsl.exe " + command)
+        info = []
+        NSEC3_flag = False
+        for line in output.strip().splitlines():
+            if 'NSEC' in line:
+                info.append(line)
+            elif 'NSEC3' in line:
+                NSEC3_flag = True
+                info.append(line)
+            elif target in line and target != (prefix + '.' + target):
+                info.append(line)
+            else:
+                continue
+
+        domains = set()
+        for line in info:
+            print('line: ', line)
+            words = line.replace('\t', ' ').split(' ')
+            for word in words:
+                word = word[:len(word) - 1]
+                if target in word and word not in domains and word != target:
+                    domains.add(word.replace(' ', '').replace(';', ''))
+
+        start = ''
+        tmp = sorted(list(domains))
+        for i in range(len(tmp)):
+            domain = tmp[i]
+            print('el: ', domain)
+        if prefix == 'z': stop_flag = True
+        dig(target, prefix, NSEC3_flag, domains, stop_flag)
+
 
 def main():
     args = parseArgs()
     domain = args[0]
-    print(domain)
+    info = dig(domain, prefix='a', NSEC3_flag=False, domains=set(), stop_flag=False)
+    domains = info[0]
+    NSEC3_flag = info[1]
+
 
 if __name__ == '__main__':
     main()
